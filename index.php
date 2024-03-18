b45afaf8c5d0f40e56a8fa3d1fc566f4e86f83be
jeanclaude <jeanclaudenzabaherahaza@gmail.com>
2024-03-18 15:38:16 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DEVOPS CAT</title>
+</head>
+<body>
+    <?php
+    echo"welcome to Student MIS";
+    ?>
+    <a href src="registration_page.php">Register</a>
+</body>
+</html>
